14c6ee079e1133d52a2e616fd6c1adac8d34f1d2
Sanjay <scshitole@gmail.com>
2021-01-29 11:11:37 -0800

    add

--- main.tf
+++ main.tf
@@ -23,5 +23,7 @@ provider "bigip" {
 
 # deploy application using as3
 resource "bigip_as3" "app1" {
-  as3_json = file(var.removeapp)
+  as3_json = file(var.declaration)
+  //as3_json = file(var.removeapp)
+
 }
